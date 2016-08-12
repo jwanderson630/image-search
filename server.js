1e903a3fd9564ddec1edeaab4f6c6bc943efa135
Joe Anderson <yoej262@gmail.com>
2016-08-12 04:48:15 +0000

    working version

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
 var express = require('express');
 var app = express();
-var googl = require('goo.gl');
-googl.setKey('AIzaSyAmO8l8hUZqY192Wu-o-ghaoVpshcl8n8c');
-googl.getKey();
-app.get(/^\/(.+)/, function(req, res) {
-  var originalUrl = req.originalUrl.slice(1,-1);
-  var finalResponse = {"originalURL" : null, "shortenedURL" : null};
-    googl.shorten(originalUrl)
-    .then(function(result){
-      console.log(originalUrl);
-      console.log(result);
-      finalResponse.originalURL = originalUrl;
-      finalResponse.shortenedURL = result;
-      res.send(finalResponse);
-    }).catch(function(err){
-      console.log(err.message);
-      finalResponse = {"error":err.message};
-      res.send(finalResponse);
-    });
+var imageSearch = require('node-google-image-search');
+var history = [];
+app.get('/search/:TERMS', function(req, res) {
+  var results = imageSearch(req.params.TERMS, function(final){
+      res.send(final);
+      history.push(req.params.TERMS);
+    }, req.query.offset, 5);
+});
+app.get('/latest', function(req,res){
+  res.send(history);
 });
 app.listen(8080, function () {
-  console.log('Url Shortening app listening on port 8080');
+  console.log('Image Search app listening on port 8080');
 });
